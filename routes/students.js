53459805a8ec273aacde51b2cd21dd71b2fc5ee8
Charlie0560 <chaitanyalokhande87@gmail.com>
2022-10-19 01:58:52 +0530

    jwt authentication

--- routes/students.js
+++ routes/students.js
@@ -1,186 +1,170 @@
 const router = require("express").Router();
 const Students = require("../models/Students");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 // const
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+const express = require("express");
+const {
+  signup,
+  login,
+  verifyToken,
+  getUser,
+  refreshToken,
+  logout,
+} = require("../controllers/Student_Controller");
 
-router.post("/signup", async (req, res) => {
-  try {
-    const { fullname, collegeId, password, rollno, div, branch } = req.body;
-    const emailRegex = /@ms.pict.edu/;
-    if (!emailRegex.test(collegeId)) throw "CollgeId is not valid";
-    if (password.length < 6) throw "Password must be of atleast 6 characters";
-    const user = await Students.findOne({ collegeId });
-    if (user) {
-      res.status(200).json("User if Already Exists");
-    }
-    const salt = await bcrypt.genSalt(10);
-    const hashedpass = await bcrypt.hash(password, salt);
-    const newStudent = new Students({
-      fullname,
-      collegeId,
-      password: hashedpass,
-      rollno,
-      div,
-      branch,
-    });
-    const student = await newStudent.save();
-    res.status(200).json(student);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// router.post("/signup", async (req, res) => {
+//   try {
+//     const { fullname, collegeId, password, rollno, div, branch } = req.body;
+//     const emailRegex = /@ms.pict.edu/;
+//     if (!emailRegex.test(collegeId)) throw "CollgeId is not valid";
+//     if (password.length < 6) throw "Password must be of atleast 6 characters";
+//     const user = await Students.findOne({ collegeId });
+//     if (user) {
+//       res.status(200).json("User if Already Exists");
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedpass = await bcrypt.hash(password, salt);
+//     const newStudent = new Students({
+//       fullname,
+//       collegeId,
+//       password: hashedpass,
+//       rollno,
+//       div,
+//       branch,
+//     });
+//     const student = await newStudent.save();
+//     res.status(200).json(student);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
 
-router.post("/login", async (req, res) => {
-  try {
-    const user = await Students.findOne({ collegeId: req.body.collegeId });
-    !user && res.status(404).send("User not found");
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    !validPassword && res.status(400).json("wrongpassword");
+// router.post("/login", async (req, res) => {
+//   try {
+//     const user = await Students.findOne({ collegeId: req.body.collegeId });
+//     !user && res.status(404).send("User not found");
+//     const validPassword = await bcrypt.compare(
+//       req.body.password,
+//       user.password
+//     );
+//     !validPassword && res.status(400).json("wrongpassword");
 
-    const token = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "15hr",
-    });
+//     const token = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, {
+//       expiresIn: "15hr",
+//     });
 
-    // console.log("Generated Token\n", token);
-    // if (req.cookies[`${user._id}`]) {
-    //   req.cookies[`${user._id}`] = "";
-    // }
-    res.cookie(String(user._id), token, {
-      path: "/",
-      expires: new Date(Date.now() + 1000 * 35),
-      httpOnly: true,
-      sameSite: "lax",
-    });
-    console.log(token);
-    res.status(200).json({ user, token });
-  } catch (err) {
-    // res.status(500).json(err);
-    console.log(err);
-  }
-});
+//     // console.log("Generated Token\n", token);
+//     // if (req.cookies[`${user._id}`]) {
+//     //   req.cookies[`${user._id}`] = "";
+//     // }
+//     res.cookie(String(user._id), token, {
+//       path: "/",
+//       expires: new Date(Date.now() + 1000 * 35),
+//       httpOnly: true,
+//       sameSite: "lax",
+//     });
+//     console.log(token);
+//     res.status(200).json({ user, token });
+//   } catch (err) {
+//     // res.status(500).json(err);
+//     console.log(err);
+//   }
+// });
 
 //logout
 
-router.post("/logout", async (req, res, next) => {
-  const cookies = req.headers.cookie;
-  const prevToken = cookies.split("=")[1];
-  if (!prevToken) {
-    return res.status(400).json({ message: "Couldn't find token" });
-  }
-  jwt.verify(String(prevToken), process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) {
-      console.log(err);
-      return res.status(403).json({ message: "Authentication failed" });
-    }
-    res.clearCookie(`${user.id}`);
-    req.cookies[`${user.id}`] = "";
-    return res.status(200).json({ message: "Successfully Logged Out" });
-  });
-});
+// router.post("/logout", async (req, res, next) => {
+//   const cookies = req.headers.cookie;
+//   const prevToken = cookies.split("=")[1];
+//   if (!prevToken) {
+//     return res.status(400).json({ message: "Couldn't find token" });
+//   }
+//   jwt.verify(String(prevToken), process.env.JWT_SECRET_KEY, (err, user) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(403).json({ message: "Authentication failed" });
+//     }
+//     res.clearCookie(`${user.id}`);
+//     req.cookies[`${user.id}`] = "";
+//     return res.status(200).json({ message: "Successfully Logged Out" });
+//   });
+// });
 //verify token
-router.get("/astudent", async (req, res) => {
-  const cookies = req.headers.cookie;
-  const token = cookies.split("=")[1];
-  console.log(token);
+// router.get("/astudent", async (req, res) => {
+//   const cookies = req.headers.cookie;
+//   const token = cookies.split("=")[1];
+//   console.log(token);
 
-  // const headers = req.headers[`authorization`];
-  // const token = headers.split(" ")[1];
-  if (!token) {
-    res.status(404).json({ message: "NO token found" });
-  }
-  jwt.verify(String(token), process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      res.status(400).json({ message: "Invalid Token" });
-    }
-    console.log(user.id);
-    req.id = user.id;
-  });
+//   // const headers = req.headers[`authorization`];
+//   // const token = headers.split(" ")[1];
+//   if (!token) {
+//     res.status(404).json({ message: "NO token found" });
+//   }
+//   jwt.verify(String(token), process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+//     if (err) {
+//       res.status(400).json({ message: "Invalid Token" });
+//     }
+//     console.log(user.id);
+//     req.id = user.id;
+//   });
 
-  const userId = req.id;
-  let user;
-  try {
-    user = await Students.findById(userId, "-password");
-  } catch (err) {
-    return new Error(err);
-  }
-  if (!user) {
-    return res.status(404).json({ message: "Student Not found" });
-  }
-  return res.status(200).json({ user });
-});
-
-const verifyToken = (req, res, next) => {
-  const cookies = req.headers.cookie;
-  const token = cookies.split("=")[1];
-  if (!token) {
-    res.status(404).json({ message: "No token found" });
-  }
-  jwt.verify(String(token), process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      return res.status(400).json({ message: "Invalid TOken" });
-    }
-    console.log(user.id);
-    req.id = user.id;
-  });
-  next();
-};
-
-const getUser = async (req, res, next) => {
-  const userId = req.id;
-  let user;
-  try {
-    user = await Students.findById(userId, "-password");
-  } catch (err) {
-    return new Error(err);
-  }
-  if (!user) {
-    return res.status(404).json({ messsage: "User Not FOund" });
-  }
-  return res.status(200).json({ user });
-};
+//   const userId = req.id;
+//   let user;
+//   try {
+//     user = await Students.findById(userId, "-password");
+//   } catch (err) {
+//     return new Error(err);
+//   }
+//   if (!user) {
+//     return res.status(404).json({ message: "Student Not found" });
+//   }
+//   return res.status(200).json({ user });
+// });
 
 // refresh token
-router.get("/refreshtoken", async (req, res, next) => {
-  const cookies = req.headers.cookie;
-  const prevtoken = cookies.split("=")[1];
-  if (!prevtoken) {
-    return res.status(400).json({ message: "Could'nt Find Token" });
-  }
-  jwt.verify(
-    (String(prevtoken), process.env.ACCESS_TOKEN_SECRET),
-    (err, user) => {
-      if (err) {
-        console.log(err);
-        return res.status(403).json({ message: "Authentication Failed" });
-      }
-      res.clearCookie(`${user.id}`);
-      req.cookies[`${user.id}`] = "";
+// router.get("/refreshtoken", async (req, res, next) => {
+//   const cookies = req.headers.cookie;
+//   const prevtoken = cookies.split("=")[1];
+//   if (!prevtoken) {
+//     return res.status(400).json({ message: "Could'nt Find Token" });
+//   }
+//   jwt.verify(
+//     (String(prevtoken), process.env.ACCESS_TOKEN_SECRET),
+//     (err, user) => {
+//       if (err) {
+//         console.log(err);
+//         return res.status(403).json({ message: "Authentication Failed" });
+//       }
+//       res.clearCookie(`${user.id}`);
+//       req.cookies[`${user.id}`] = "";
 
-      const token = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "15hr",
-      });
-      console.log("RE-Generated Token\n", token);
-      res.cookie(String(user.id), token, {
-        path: "/",
-        expires: new Date(Date.now() + 1000 * 30),
-        httpOnly: true,
-        sameSite: "lax",
-      });
+//       const token = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET, {
+//         expiresIn: "15hr",
+//       });
+//       console.log("RE-Generated Token\n", token);
+//       res.cookie(String(user.id), token, {
+//         path: "/",
+//         expires: new Date(Date.now() + 1000 * 30),
+//         httpOnly: true,
+//         sameSite: "lax",
+//       });
 
-      req.id = user.id;
-    }
-  );
+//       req.id = user.id;
+//     }
+//   );
 
-  verifyToken();
-  getUser();
-});
+//   verifyToken();
+//   getUser();
+// });
 
 // router.get("/refresh",)
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/user", verifyToken, getUser);
+router.get("/refresh", refreshToken, verifyToken, getUser);
+router.post("/logout", verifyToken, logout);
 
 router.get("/getstudent", async (req, res) => {
   const userId = req.id;
